3d70580318f25a8b7b72c782489fe5e7260adb2e
Bastien Coutadeur <bastien.coutadeur@epita.fr>
2022-07-15 20:30:07 +0200

    added lsp keybindings

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -3,40 +3,43 @@ local lsp = require "lspconfig"
 
 -- enable keybindings only when an lsp is running
 local function lsp_keymaps(bufnr)
-    local opts = { noremap = true, silent = true }
-    -- vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-    -- -- vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-    -- vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-    -- vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-    -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-    --LSP renaming (more intelligne tthan :%s//g)
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "grn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-    -- Creates a loclist of the function references 
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-    -- -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-    -- -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>f", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
-    -- vim.api.nvim_buf_set_keymap(bufnr, "n", "[d", '<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>', opts)
-    -- vim.api.nvim_buf_set_keymap(
-    --     bufnr,
-    -- "n",
-    -- "gl",
-    -- '<cmd>lua vim.diagnostic.open_float({ border = "rounded" })<CR>',
-    -- opts
-    -- )
-    -- vim.api.nvim_buf_set_keymap(bufnr, "n", "]d", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', opts)
-    -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
-    vim.cmd([[ command! Format execute 'lua vim.lsp.buf.format{async=true}' ]])
+    -- noremap => remaps the keybinding remving older map, silent=true => doesn't displays the sequence, buffer = bufnr => only apllies mappiing to current buffer
+    local bufopts = { noremap = true, silent = true, buffer = bufnr }
+
+    -- go to definition (like with ctags)
+    vim.keymap.set("n", "<C-]>", vim.lsp.buf.definition, bufopts)
+    -- vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+    -- vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+
+    -- Generate references
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+    -- displays function information (overrides the documentation opener)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+    -- vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+    -- vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+    -- vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+    -- vim.keymap.set('n', '<space>wl', function()
+    --   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    -- end, bufopts)
+    -- vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+    -- generates action
+    vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+    -- formats the current buffer
+    vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
+    -- vim.cmd([[ command! Format execute 'lua vim.lsp.buf.format{async=true}' ]])
 end
 
-on_attach = function (client, bufnr)
+on_attach = function(client, bufnr)
     lsp_keymaps(bufnr)
 end
 
 lsp.clangd.setup(coq.lsp_ensure_capabilities({
-     on_attach = on_attach,
+    on_attach = on_attach,
 }))
 
-require'lspconfig'.asm_lsp.setup(coq.lsp_ensure_capabilities())
+require 'lspconfig'.asm_lsp.setup(coq.lsp_ensure_capabilities())
 
 lsp.pylsp.setup(coq.lsp_ensure_capabilities({
     on_attach = on_attach
@@ -51,7 +54,7 @@ lsp.sumneko_lua.setup(coq.lsp_ensure_capabilities({
             },
             diagnostics = {
                 -- Get the language server to recognize the `vim` global
-                globals = {'vim', 'use'},
+                globals = { 'vim', 'use' },
             },
             workspace = {
                 -- Make the server aware of Neovim runtime files
@@ -62,4 +65,5 @@ lsp.sumneko_lua.setup(coq.lsp_ensure_capabilities({
             },
         },
     },
+    on_attach = on_attach
 }))
--- lua/config/terminal.lua
+++ lua/config/terminal.lua
@@ -1,4 +1,6 @@
 -- allows escape to exit terminal mode
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
 -- open split terminal with ;t
-vim.keymap.set("n", "gt", ":sp term://zsh<CR> <C-w>r 12<C-W>- i")
+vim.keymap.set("n", ";t", ":sp term://zsh<CR> <C-w>r 12<C-W>- i")
+-- clears highlight with esc
+vim.keymap.set("n", "<Esc><Esc>", ':let @/=""<CR>')
